246f2552fc5cc6649b371bcd0e5b42347c0ecf71
Antoniolol195 <89756692+Antoniolol195@users.noreply.github.com>
2021-08-30 16:14:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function jogos_click(){
+document.getElementById("div2").style.display="block";
+document.getElementById("div1").style.display="none";
+document.getElementById("div3").style.display="none";
+}
+function sobre_click(){
+document.getElementById("div1").style.display="block";
+document.getElementById("div2").style.display="none";
+document.getElementById("div3").style.display="none";
+}
+function dev_click(){
+document.getElementById("div1").style.display="none";
+document.getElementById("div2").style.display="none";
+document.getElementById("div3").style.display="block";
+}
